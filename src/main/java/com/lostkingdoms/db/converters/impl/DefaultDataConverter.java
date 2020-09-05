503b24b7b02560af59efc3e87183b39e140bfd18
Tim Küchler <tim.kuechler@online.de>
2020-09-05 13:43:28 +0200

    05.09.2020 13:43
    Added conversion from database

--- src/main/java/com/lostkingdoms/db/converters/impl/DefaultDataConverter.java
+++ src/main/java/com/lostkingdoms/db/converters/impl/DefaultDataConverter.java
@@ -1,5 +1,6 @@
 package com.lostkingdoms.db.converters.impl;
 
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,8 +20,49 @@ import com.lostkingdoms.db.organization.DataOrganizationManager;
  */
 public final class DefaultDataConverter<T> extends AbstractDataConverter<T> {
 
+	/**
+	 * The generic class of the first map type argument or the list type
+	 */
+	private Class<?> genericClass1;
+	
+	/**
+	 * The generic class of the second map type argument
+	 */
+	private Class<?> genericClass2;
+	
+	
+	
+	/**
+	 * Constructor for non-map and non-list {@link DefaultDataConverter}
+	 * 
+	 * @param thisClass
+	 */
 	public DefaultDataConverter(Class<T> thisClass) {
 		super(thisClass);
+	}
+	
+	/**
+	 * Constructor for list {@link DefaultDataConverter}
+	 * 
+	 * @param thisClass
+	 * @param genericClass1
+	 */
+	public DefaultDataConverter(Class<T> thisClass, Class<?> genericClass) {
+		super(thisClass);
+		this.genericClass1 = genericClass;
+	}
+	
+	/**
+	 * Constructor for map {@link DefaultDataConverter}
+	 * 
+	 * @param thisClass
+	 * @param genericClass1
+	 * @param genericClass2
+	 */
+	public DefaultDataConverter(Class<T> thisClass, Class<?> genericClass1, Class<?> genericClass2) {
+		super(thisClass);
+		this.genericClass1 = genericClass1;
+		this.genericClass2 = genericClass2;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -34,10 +76,59 @@ public final class DefaultDataConverter<T> extends AbstractDataConverter<T> {
 		if(dataOrganizationManager.hasDataConverter(getThisClass())) {
 			return ((AbstractDataConverter<T>)dataOrganizationManager.getDataConverter(getThisClass())).convertFromDatabase(s);
 		}
+		
+		//Convert object from json
+		Object obj = null;
+		try {
+			obj = gson.fromJson(s, getThisClass());
+		} catch (Exception e) {
+			if(genericClass2 != null) {
+				Map<String, String> temp = new HashMap<String, String>();
+				obj = gson.fromJson(s, temp.getClass());
+			} else {
+				List<String> temp = new ArrayList<String>();
+				obj = gson.fromJson(s, temp.getClass());
+			}
+		}
+		
+		//Object is list
+		if(obj instanceof List) {
+			List<Object> newList = new ArrayList<Object>();
+			
+			for(String o : ((List<String>)obj)) {
+				if(dataOrganizationManager.hasDataConverter(this.genericClass1)) {
+					AbstractDataConverter<?> converter = dataOrganizationManager.getDataConverter(this.genericClass1);
+					newList.add(converter.convertFromDatabase(o));
+				}
+			}
+			
+			if(newList.size() > 0) return (T) ((Object) newList);
+		}
 
-		//TODO if List or map contains OrganizedObjects
+		//Object is map
+		if(obj instanceof Map) {
+			Map<Object, Object> newMap = new HashMap<Object, Object>();
+			
+			for(Entry<String, String> entry: ((Map<String, String>)obj).entrySet()) {
+				if(dataOrganizationManager.hasDataConverter(genericClass1) &&
+						dataOrganizationManager.hasDataConverter(genericClass2)) {
+					newMap.put(dataOrganizationManager.getDataConverter(genericClass1).convertFromDatabase(entry.getKey())
+							, dataOrganizationManager.getDataConverter(genericClass2).convertFromDatabase(entry.getValue()));
+				} else
+				if(dataOrganizationManager.hasDataConverter(genericClass1)) {
+					newMap.put(dataOrganizationManager.getDataConverter(genericClass1).convertFromDatabase(entry.getKey())
+							, gson.fromJson(entry.getValue(), genericClass2));
+				} else
+				if(dataOrganizationManager.hasDataConverter(genericClass2)){
+					newMap.put(gson.fromJson(entry.getKey(), genericClass1)
+							, dataOrganizationManager.getDataConverter(genericClass2).convertFromDatabase(entry.getValue()));
+				}
+			}
+			
+			if(newMap.size() > 0) return (T) ((Object) newMap);
+		}
 
-		return gson.fromJson(s, getThisClass());
+		 return (T) obj;
 
 	}
 
