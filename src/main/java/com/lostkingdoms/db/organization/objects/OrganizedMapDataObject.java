6dc7a762dd783f15d6ae0f7b6e123d652d3fff18
Tim Küchler <account@tim-kuechler.de>
2022-04-14 20:52:38 +0200

    Bugfix

--- src/main/java/com/lostkingdoms/db/organization/objects/OrganizedMapDataObject.java
+++ src/main/java/com/lostkingdoms/db/organization/objects/OrganizedMapDataObject.java
@@ -150,67 +150,69 @@ public final class OrganizedMapDataObject<K, V> extends OrganizedDataObject<Hash
         //Update the timestamp for last change
         updateTimestamp(newTimestamp);
 
-        try (Jedis jedis = JedisFactory.getInstance().getJedis()) {
-            //Get the data key
-            DataKey dataKey = getDataKey();
+        new Thread(() -> {
+            try (Jedis jedis = JedisFactory.getInstance().getJedis()) {
+                //Get the data key
+                DataKey dataKey = getDataKey();
 
-            //Conversion to redis and mongoDB
-            String dataString = converter.convertToDatabase(map);
-            if (dataString == null) {
-                return;
-            }
-
-            //Update to redis
-            if (getOrganizationType() == OrganizationType.SYNC || getOrganizationType() == OrganizationType.BOTH) {
-                if (dataString.equals("")) {
-                    jedis.del(dataKey.getRedisKey());
-                } else {
-                    jedis.set(dataKey.getRedisKey(), dataString);
+                //Conversion to redis and mongoDB
+                String dataString = converter.convertToDatabase(map);
+                if (dataString == null) {
+                    return;
                 }
-            }
 
-            //Update to MongoDB
-            if (getOrganizationType() == OrganizationType.SAVE_TO_DB || getOrganizationType() == OrganizationType.BOTH) {
-                DB mongoDB = MongoDBFactory.getInstance().getMongoDatabase();
-                DBCollection collection = mongoDB.getCollection(dataKey.getMongoDBCollection());
-
-                while (true) {
-                    try {
-                        //Test if object already exists
-                        BasicDBObject query = new BasicDBObject();
-                        query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-
-                        DBObject object = collection.findOne(query);
-                        if (object != null) {
-                            query = new BasicDBObject();
-                            query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-
-                            BasicDBObject newDoc = new BasicDBObject();
-                            newDoc.put(dataKey.getMongoDBValue(), dataString);
-
-                            BasicDBObject update = new BasicDBObject();
-                            update.put("$set", newDoc);
-
-                            collection.update(query, update);
-                        } else {
-                            BasicDBObject create = new BasicDBObject();
-                            create.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-                            create.put(MONGO_IDENTIFIER, dataKey.getMongoDBIdentifier());
-                            create.put(dataKey.getMongoDBValue(), dataString);
-
-                            collection.insert(create);
-                        }
-
-                        break;
-                    } catch (DuplicateKeyException ignored) {
+                //Update to redis
+                if (getOrganizationType() == OrganizationType.SYNC || getOrganizationType() == OrganizationType.BOTH) {
+                    if (dataString.equals("")) {
+                        jedis.del(dataKey.getRedisKey());
+                    } else {
+                        jedis.set(dataKey.getRedisKey(), dataString);
                     }
                 }
 
-            }
+                //Update to MongoDB
+                if (getOrganizationType() == OrganizationType.SAVE_TO_DB || getOrganizationType() == OrganizationType.BOTH) {
+                    DB mongoDB = MongoDBFactory.getInstance().getMongoDatabase();
+                    DBCollection collection = mongoDB.getCollection(dataKey.getMongoDBCollection());
 
-            //Publish to other servers via redis
-            sendSyncMessage(jedis);
-        }
+                    while (true) {
+                        try {
+                            //Test if object already exists
+                            BasicDBObject query = new BasicDBObject();
+                            query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+
+                            DBObject object = collection.findOne(query);
+                            if (object != null) {
+                                query = new BasicDBObject();
+                                query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+
+                                BasicDBObject newDoc = new BasicDBObject();
+                                newDoc.put(dataKey.getMongoDBValue(), dataString);
+
+                                BasicDBObject update = new BasicDBObject();
+                                update.put("$set", newDoc);
+
+                                collection.update(query, update);
+                            } else {
+                                BasicDBObject create = new BasicDBObject();
+                                create.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+                                create.put(MONGO_IDENTIFIER, dataKey.getMongoDBIdentifier());
+                                create.put(dataKey.getMongoDBValue(), dataString);
+
+                                collection.insert(create);
+                            }
+
+                            break;
+                        } catch (DuplicateKeyException ignored) {
+                        }
+                    }
+
+                }
+
+                //Publish to other servers via redis
+                sendSyncMessage(jedis);
+            }
+        }).start();
 
         //Set the local data
         setData(map);
--- src/main/java/com/lostkingdoms/db/organization/objects/OrganizedSingleDataObject.java
+++ src/main/java/com/lostkingdoms/db/organization/objects/OrganizedSingleDataObject.java
@@ -151,80 +151,81 @@ public final class OrganizedSingleDataObject<T> extends OrganizedDataObject<T> {
         updateTimestamp(newTimestamp);
 
         // Send to Cache and DB
-        try (Jedis jedis = JedisFactory.getInstance().getJedis()) {
+        new Thread(() -> {
+            try (Jedis jedis = JedisFactory.getInstance().getJedis()) {
 
-            //Get the data key
-            DataKey dataKey = getDataKey();
+                //Get the data key
+                DataKey dataKey = getDataKey();
 
-            //Conversion to redis and mongoDB
-            String dataString = converter.convertToDatabase(data);
-            if (dataString == null) {
-                return;
-            }
-
-            //Update to redis
-            if (getOrganizationType() == OrganizationType.SYNC || getOrganizationType() == OrganizationType.BOTH) {
-                if (dataString.equals("")) {
-                    jedis.del(dataKey.getRedisKey());
-                } else {
-                    jedis.set(dataKey.getRedisKey(), dataString);
+                //Conversion to redis and mongoDB
+                String dataString = converter.convertToDatabase(data);
+                if (dataString == null) {
+                    return;
                 }
-            }
 
-            //Update to MongoDB
-            if (getOrganizationType() == OrganizationType.SAVE_TO_DB || getOrganizationType() == OrganizationType.BOTH) {
-                DB mongoDB = MongoDBFactory.getInstance().getMongoDatabase();
-                DBCollection collection = mongoDB.getCollection(dataKey.getMongoDBCollection());
-
-                while (true) {
-                    try {
-                        //Test if object already exists
-                        BasicDBObject query = new BasicDBObject();
-                        query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-
-                        DBObject object = collection.findOne(query);
-
-                        if (object != null) {
-                            query = new BasicDBObject();
-                            query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-
-                            BasicDBObject update;
-                            if (dataString.equals("")) {
-                                BasicDBObject newDoc = new BasicDBObject();
-                                newDoc.put(dataKey.getMongoDBValue(), "");
-
-                                update = new BasicDBObject();
-                                update.put("$unset", newDoc);
-                            } else {
-                                BasicDBObject newDoc = new BasicDBObject();
-                                newDoc.put(dataKey.getMongoDBValue(), dataString);
-
-                                update = new BasicDBObject();
-                                update.put("$set", newDoc);
-                            }
-
-                            collection.update(query, update);
-                        } else {
-                            if (!dataString.equals("")) {
-                                BasicDBObject create = new BasicDBObject();
-                                create.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
-                                create.put(MONGO_IDENTIFIER, dataKey.getMongoDBIdentifier());
-                                create.put(dataKey.getMongoDBValue(), dataString);
-
-                                collection.insert(create);
-                            }
-                        }
-
-                        break;
-                    } catch (DuplicateKeyException ignored) {
+                //Update to redis
+                if (getOrganizationType() == OrganizationType.SYNC || getOrganizationType() == OrganizationType.BOTH) {
+                    if (dataString.equals("")) {
+                        jedis.del(dataKey.getRedisKey());
+                    } else {
+                        jedis.set(dataKey.getRedisKey(), dataString);
                     }
                 }
+
+                //Update to MongoDB
+                if (getOrganizationType() == OrganizationType.SAVE_TO_DB || getOrganizationType() == OrganizationType.BOTH) {
+                    DB mongoDB = MongoDBFactory.getInstance().getMongoDatabase();
+                    DBCollection collection = mongoDB.getCollection(dataKey.getMongoDBCollection());
+
+                    while (true) {
+                        try {
+                            //Test if object already exists
+                            BasicDBObject query = new BasicDBObject();
+                            query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+
+                            DBObject object = collection.findOne(query);
+
+                            if (object != null) {
+                                query = new BasicDBObject();
+                                query.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+
+                                BasicDBObject update;
+                                if (dataString.equals("")) {
+                                    BasicDBObject newDoc = new BasicDBObject();
+                                    newDoc.put(dataKey.getMongoDBValue(), "");
+
+                                    update = new BasicDBObject();
+                                    update.put("$unset", newDoc);
+                                } else {
+                                    BasicDBObject newDoc = new BasicDBObject();
+                                    newDoc.put(dataKey.getMongoDBValue(), dataString);
+
+                                    update = new BasicDBObject();
+                                    update.put("$set", newDoc);
+                                }
+
+                                collection.update(query, update);
+                            } else {
+                                if (!dataString.equals("")) {
+                                    BasicDBObject create = new BasicDBObject();
+                                    create.put(IDENTIFIER, dataKey.getMongoDBIdentifier());
+                                    create.put(MONGO_IDENTIFIER, dataKey.getMongoDBIdentifier());
+                                    create.put(dataKey.getMongoDBValue(), dataString);
+
+                                    collection.insert(create);
+                                }
+                            }
+
+                            break;
+                        } catch (DuplicateKeyException ignored) {
+                        }
+                    }
+                }
+
+                //Publish to other servers via redis
+                sendSyncMessage(jedis);
             }
-
-            //Publish to other servers via redis
-            sendSyncMessage(jedis);
-        }
-
+        }).start();
 
         //Set the local data
         setData(data);
